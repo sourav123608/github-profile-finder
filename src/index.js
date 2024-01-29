e33c7268c0dfe883a560d62a9a77b6e1a5cad3bf
SOURAV KUMAR PANDA <74825918+sourav123608@users.noreply.github.com>
2024-01-29 08:42:24 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import GithubProfileFinder from "./github-profile-finder";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <GithubProfileFinder />
+  </React.StrictMode>
+);
+
+
